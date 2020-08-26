af33d8a9458a7c899d23cc9c4caf4a4c365bb7e6
akehn24 <32083624+akehn24@users.noreply.github.com>
2020-08-26 15:04:36 -0700

    Update Pieces and File headers
    Missed Pieces.h in the last push. Pieces.h now written and headers added to all of the files.

--- Pieces.h
+++ Pieces.h
@@ -1,1 +1,21 @@
 #pragma once
+#ifndef _PIECES_
+#define _PIECES_
+
+
+/*
+* ------------------------------------------------------------------*
+*						Pieces Header File							*
+* ------------------------------------------------------------------*
+*/
+
+class Pieces {
+public:
+
+	int GetBlockType(int pPiece, int pRotation, int pX, int pY);
+	int GetXInitPostition(int pPiece, int pRotation);
+	int GetYInitPosition(int pPiece, int pRotation);
+};
+
+#endif 
+
