d9cbdef45053eac33b33bc74807d0a54b59362f0
akehn24 <32083624+akehn24@users.noreply.github.com>
2020-09-01 22:03:24 -0700

    Game file skeletons
    Created Game.h and a skeleton for Game.cpp.

--- Game.cpp
+++ Game.cpp
@@ -8,3 +8,75 @@
 *						   Game CPP File							*
 * ------------------------------------------------------------------*
 */
+
+
+/*
+* =========================================================================
+* Gets a random int between the two provided numbers.
+* 
+* Parameters:
+* 
+* >> pA:			1st number
+* >> pB:			2nd number
+* =========================================================================
+*/
+int Game::GetRand(int pA, int pB) {
+	return 0;
+}
+
+/*
+* =========================================================================
+* The initial parameters of the game to be made.
+* =========================================================================
+*/
+void Game::InitGame() {
+
+}
+
+/*
+* =========================================================================
+* Sets the upcoming piece as the active one and selects and stores the 
+* upcoming piece.
+* =========================================================================
+*/
+void CreateNewPiece() {
+
+}
+
+/*
+* =========================================================================
+* Draws each block of the piece by iterating through the piece matrix. Calls
+* DrawRectangle from IO to draw the rectangles.
+* 
+*	Green = normal block
+*	Blue  = pivot block
+* 
+* Parameters:
+* 
+* >> pX:			horizontal position in blocks
+* >> pY:			vertical position in blocks
+* >> pPiece:		the piece to draw
+* >> pRotation:		1 of 4 possible rotations
+* =========================================================================
+*/
+void Game::DrawPiece(int pX, int pY, int pPiece, int pRotation) {
+
+}
+
+/*
+* =========================================================================
+* Draws the board for the current Tetris game.
+* =========================================================================
+*/
+void Game::DrawBoard() {
+
+}
+
+/*
+* =========================================================================
+* Draws the entire game by calling the above methods.
+* =========================================================================
+*/
+void DrawScene() {
+
+}
